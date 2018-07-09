4b68ea3959918c06f82d5a480230289d515b11c8
ugochinyere1 <ugochinyere.okogeri@csedge.org>
2018-07-09 16:37:05 -0400

    quick math

--- math-practice.js
+++ math-practice.js
@@ -1,3 +1,4 @@
+//ugochinyere okogeri
 // Under each question, write the code that solves it, and then write your answer in a comment
 // For example, if the question is: What's the sum of 79 and 2018 you would write:
 console.log(79 + 2018);
@@ -10,12 +11,23 @@ const r = 25;
 
 // 1. Is 91 divisible by 13? (The answer should be either true or false)
 
+var quotient= 91%13;
+console.log(0==quotient);
 // 2. What is the result when the q divided by r is rounded to the nearest integer?
-
+var q;
+var r;
+var result= q/r;
+console.log(result==math.round);
 // 3. Is the product of p and r greater than q squared? (The answer should be either true or false)
+var q;
+var r;
+var product= p*r;
+var productExp= q**;
+console.log(product>productExp);
 
 // 4. What is the remainder when the sum of q and r is divided by p?
-
+var q;
+var
 // 5. 27 squared is equal to what number raised to the third power?
 
 // 6. What is the absolute value of p - q? (Find out how to do absolute value by searching online.)
